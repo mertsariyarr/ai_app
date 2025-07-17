2e1b0984f4e777c9892abef6523ad63c9f0d1ab8
mertsariyar <mertsariyar60@gmail.com>
2025-07-17 21:32:32 +0300

    added ai functionality

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+
 def main():
-    print("Hello from ai-app!")
+    generated_content = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    )
+    
+    text = generated_content._get_text()
+
+    print(text)
+    print(f"Prompt tokens: {generated_content.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {generated_content.usage_metadata.candidates_token_count}")
+
 
 
 if __name__ == "__main__":
