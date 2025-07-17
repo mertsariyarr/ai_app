c15911925fc95c2bff43c3b959bd64218a62e5fc
mertsariyar <mertsariyar60@gmail.com>
2025-07-17 21:55:20 +0300

    added new user prompt to ai

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
 
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
 
 
 def main():
-    generated_content = client.models.generate_content(
+    load_dotenv()
+    args = sys.argv[1:]
+
+    if not args:
+        print("Give us something to respond!")
+        sys.exit(1)
+
+    user_prompt = " ".join(args)
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=user_prompt
     )
     
-    text = generated_content._get_text()
+    text = response._get_text()
 
     print(text)
-    print(f"Prompt tokens: {generated_content.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {generated_content.usage_metadata.candidates_token_count}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 
