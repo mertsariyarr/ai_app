d62bd1154ccc60db978b8b190f25ba8366179cf1
mertsariyar <mertsariyar60@gmail.com>
2025-07-19 15:32:49 +0300

    added run a python file functionality to our ai by creating run_python

---
+++ functions/run_python.py
@@ -0,0 +1,44 @@
+import os
+import subprocess
+
+# It's time to build the functionality for our Agent to run arbitrary Python code.
+
+def run_python_file(working_directory, file_path, args=[]):
+    abs_path = os.path.abspath(working_directory)
+    target_path = os.path.abspath(os.path.join(working_directory, file_path))
+
+    if not target_path.startswith(abs_path):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    if not os.path.exists(target_path):
+        return f'Error: File "{file_path}" not found.'
+    if not target_path.endswith(".py"):
+        return f'Error: "{file_path}" is not a Python file.'
+    
+    try:
+        commands = ["python3", target_path]
+
+        if args:
+            commands.extend(args)
+        result = subprocess.run(
+            commands,
+            capture_output=True,
+            text=True,
+            timeout=30,
+            cwd=working_directory
+        )
+        output= []
+
+        if result.stdout:
+            output.append(f'STDOUT:\n{result.stdout}')
+        if result.stderr:
+            output.append(f'STDERR:\n{result.stderr}')
+
+        if result.returncode != 0:
+            output.append(f"Process exited with code {result.returncode}")
+        
+        return "\n".join(output) if output else "No output produced."
+
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
+        
+      
--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
 from functions.write_file import write_file
+from functions.run_python import run_python_file
 
 def test():
     # result1 = get_files_info("calculator", ".")
@@ -24,10 +25,19 @@ def test():
 
     # print(result, resul1, result2, result3,result4)
 
-    result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
-    result2 = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
-    result3 = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
+    # result = write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum")
+    # result2 = write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet")
+    # result3 = write_file("calculator", "/tmp/temp.txt", "this should not be allowed")
 
-    print(result, result2, result3)
+    # print(result, result2, result3)
+    ###
+
+    result = run_python_file("calculator", "main.py")
+    result2 = run_python_file("calculator", "main.py", ["3 + 5"])
+    result3= run_python_file("calculator", "tests.py")
+    result4 = run_python_file("calculator", "../main.py")
+    result5 = run_python_file("calculator", "nonexistent.py")
+    print(result, result2, result3, result4,result5)
+
 if __name__ == "__main__":
     test()
